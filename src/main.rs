153b2b233204351b2675bf6d25e316b1bcd5caf2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-07-08 20:38:30 -0400

    Encrypt a basic state, the correct contents can be encoded later

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate actix;
 extern crate actix_web;
+extern crate base64;
 extern crate dotenv;
 extern crate env_logger;
 extern crate hyper;
 extern crate rand;
+extern crate ring;
 extern crate url;
 
 #[macro_use]
@@ -15,6 +17,8 @@ use actix_web::middleware::session::{CookieSessionBackend, SessionStorage, Reque
 use dotenv::dotenv;
 use url::Url;
 use rand::prelude::*;
+use ring::aead;
+use ring::rand::{SecureRandom, SystemRandom};
 
 fn index(req: HttpRequest<AppState>) -> &'static str {
     match req.session().get::<String>("sid") {
@@ -43,18 +47,33 @@ fn login_redirect(req: HttpRequest<AppState>) -> Result<HttpResponse> {
 
     let callback_url = req.url_for_static("callback")?;
 
-    let nonce: String = rand::thread_rng().sample_iter(&rand::distributions::Alphanumeric).take(12).collect();
+    // TODO: I may want to use the SystemRandom for this as well, but it's probably fine
+    let auth_nonce: String = rand::thread_rng().sample_iter(&rand::distributions::Alphanumeric).take(12).collect();
     // TODO: Need to handle this failure potential
-    req.session().set("auth_nonce", nonce.clone())?;
+    req.session().set("auth_nonce", auth_nonce.clone())?;
 
-    // TODO: Generate a signed websafe base 64, should include timestamp and
-    // user's session ID. Could just be encrypted AEAD preferred.
-    let state = "signed-data";
+    let mut state = b"signed-data".to_vec();
+    // Need to make room for the signature in the structure
+    for _ in 0..aead::CHACHA20_POLY1305.tag_len() { state.push(0); }
+
+    let additional_data: [u8; 0] = [];
+
+    // Generate a fresh encryption nonce, this must be separate from the auth_nonce
+    let mut nonce = vec![0; 12];
+    let rand = SystemRandom::new();
+    rand.fill(&mut nonce).unwrap();
+
+    // Generate the key we're going to use for sealing
+    let raw_key = req.state().session_key.as_bytes();
+    let sealing_key = aead::SealingKey::new(&aead::CHACHA20_POLY1305, &raw_key[..]).unwrap();
+
+    aead::seal_in_place(&sealing_key, &nonce, &additional_data, &mut state, aead::CHACHA20_POLY1305.tag_len()).unwrap();
+    let safe_state = base64::encode_config(&state, base64::URL_SAFE_NO_PAD);
 
     let auth_url = Url::parse_with_params("https://id.twitch.tv/oauth2/authorize",
-        &[("client_id", req.state().twitch_client_id.clone()), ("nonce", nonce.to_string()),
+        &[("client_id", req.state().twitch_client_id.clone()), ("nonce", auth_nonce.to_string()),
           ("redirect_uri", callback_url.as_str().to_string()), ("response_type", "code".to_string()),
-          ("scope", "openid channel_editor chat_login".to_string()), ("state", state.to_string())]
+          ("scope", "openid channel_editor chat_login".to_string()), ("state", safe_state)]
       )?;
 
     Ok(HttpResponse::Found()
@@ -68,6 +87,7 @@ fn oauth_callback(_req: HttpRequest<AppState>) -> &'static str {
 
 #[derive(Clone, Debug)]
 struct AppState {
+    session_key: String,
     twitch_client_id: String,
     twitch_client_secret: String,
 }
@@ -75,6 +95,7 @@ struct AppState {
 impl AppState {
     fn from_env() -> AppState {
         AppState {
+            session_key: std::env::var("COOKIE_SESSION_KEY").unwrap(),
             twitch_client_id: std::env::var("TWITCH_OAUTH_CLIENT_ID").unwrap(),
             twitch_client_secret: std::env::var("TWITCH_OAUTH_CLIENT_SECRET").unwrap(),
         }
@@ -85,19 +106,18 @@ fn main() {
     dotenv().ok();
     env_logger::init();
 
-    let session_key = std::env::var("COOKIE_SESSION_KEY").unwrap();
     let state = AppState::from_env();
-
     let sys = actix::System::new("stream_manager");
 
     server::new(move || {
         let cloned_state = state.clone();
+        let cookie_key = cloned_state.session_key.clone();
 
         App::with_state(cloned_state)
             .middleware(middleware::Logger::default())
             .middleware(
                 SessionStorage::new(
-                    CookieSessionBackend::private(session_key.as_bytes())
+                    CookieSessionBackend::private(&cookie_key.as_bytes())
                         .name("sm_ses")
                         // Should be true in production
                         .secure(false)
